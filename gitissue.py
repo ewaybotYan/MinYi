636bf72354ee2d8d63b44ba2c7938a6986c8e8c7
Leslie Zhu <pythonisland@gmail.com>
2015-11-30 23:50:57 +0800

    Refactor gitissue to split as two functional: dumpIssue,dumpMilstone

--- gitissue.py
+++ gitissue.py
@@ -16,8 +16,10 @@ import urllib2
 import json
 import glob,subprocess
 
+__author__ = "Leslie Zhu (pythonisland@gmail.com)"
+__version__ = "1.0.0"
 
-def dumpIssues(state="open", repo=u"MinYi", user=u"LeslieZhu"):
+def gitIssues(state="open", repo=u"MinYi", user=u"LeslieZhu"):
     """
     根据GitHub用户名、项目名、Issue状态条件，批量备份Issues.
     """
@@ -30,61 +32,99 @@ def dumpIssues(state="open", repo=u"MinYi", user=u"LeslieZhu"):
     issues = json.loads(content.read())
     
     for issue in issues:
-        # [u'body', u'labels', u'locked', u'title', u'url', u'labels_url', u'created_at', u'events_url', \
-        #    u'comments_url', u'html_url', u'comments', u'number', u'updated_at', u'assignee', u'state', \
-        #    u'user', u'milestone', u'closed_at', u'id']
-
-        title = issue[u"title"]
-        number = issue[u"number"]
+        dumpIssues(issue)
         
-        label = issue[u"labels"][0][u"name"]
-        label_folder = u"issues/%s" % (label,)
-        
-        if not os.path.exists(label_folder):
-            os.makedirs(label_folder)
+        if issue.has_key(u"milestone"):
+            dumpMilestones(issue[u"milestone"])
 
-        created_at = issue[u"created_at"][:10]
-        filename = u"%s/#%s-%s-%s.md" % (label_folder,number,created_at,title,) # Warn: 可能本地文件名会有点乱码，但在GitHub网站则会显示正常。
+def dumpIssues(issue):
+    """
+    issue是一个字典，如:
 
-        # 当标题修改了，导致文件名也修改，防止一个issue保存了多个文件
-        for issuefile in glob.glob(u"issues/*/#%s-*.md" % (number,)):
-            if issuefile != filename:
-                subprocess.Popen(["git","mv","-f",issuefile,filename])
+    {
+      "number": 2,
+      "title": "",
+      "labels": [
+            {
+               "name": "iOS",
+            }
+       ],
+      "state": "open",
+      "milestone": {
+                   },
+      "comments": 0,
+      "created_at": "2015-07-22T13:47:32Z",
+      "updated_at": "2015-11-30T09:33:16Z",
+      "closed_at": null,
+      "body": ""
+    },
+    """
+    
+    title = issue[u"title"]
+    number = issue[u"number"]
+    
+    label = issue[u"labels"][0][u"name"]
+    label_folder = u"issues/%s" % (label,)
+    created_at = issue[u"created_at"][:10]
+    
+    if not os.path.exists(label_folder):
+        os.makedirs(label_folder)
+        
+    filename = u"%s/#%s-%s-%s.md" % (label_folder,number,created_at,title,) # Warn: 可能本地文件名会有点乱码，但在GitHub网站则会显示正常。
+    
+    # 当标题修改了，导致文件名也修改，防止一个issue保存了多个文件
+    for issuefile in glob.glob(u"issues/*/#%s-*.md" % (number,)):
+        if issuefile != filename:
+            subprocess.Popen(["git","mv","-f",issuefile,filename])
 
         
-        content = issue[u"body"]
+    content = issue[u"body"].encode("utf-8")
+    
+    print filename
+    cout = open(filename,"w")
+    cout.write(content + '\n')
+    cout.close()
+
+
+def dumpMilestones(milestone):
+    """
+    milestone是一个字典结构，如:
+
+    {
+      "number": 1,
+      "title": "before 2015",
+      "description": "",
+      "open_issues": 7,
+      "closed_issues": 0,
+      "state": "open",
+      ...
+    },
+    """
         
-        print filename
-        cout = open(filename,"w")
-        cout.write(content.encode("utf-8") + '\n')
+    # 如果里程碑已经关闭，则默认不会更新里程碑的定义
+    if milestone[u"state"] != "open": return
+        
+    milestone_num = milestone[u"number"]
+    milestone_name = milestone[u"title"]
+    milestone_file = u"milestones/#%s-%s.md" % (milestone_num,milestone_name,)
+    
+    # 当标题修改了，导致文件名也修改，防止一个issue保存了多个文件
+    for milestonefile in glob.glob(u"milestones/#%s-*.md" % (milestone_num,)):
+        if milestonefile != milestone_file:
+            subprocess.Popen(["git","mv","-f",milestonefile,milestone_file])
+            
+    if milestone.has_key(u"description"):
+        milestone_description = milestone[u"description"].encode("utf-8")
+    else:
+        milestone_description = u""
+        
+    if not os.path.exists(milestone_file) or os.stat(milestone_file).st_size != len(milestone_description + '\n'):
+        print milestone_file
+        cout = open(milestone_file,"w")
+        cout.write(milestone_description + '\n')
         cout.close()
-
-
-        milestone = issue[u"milestone"]
-        milestone_num = milestone[u"number"]
-        milestone_name = milestone[u"title"]
-        
-        if milestone.has_key(u"description"):
-            milestone_description = milestone[u"description"]
-        else:
-            milestone_description = u""
-
-        milestone_file = u"milestones/#%s-%s.md" % (milestone_num,milestone_name,)
-
-        # 当标题修改了，导致文件名也修改，防止一个issue保存了多个文件
-        for milestonefile in glob.glob(u"milestones/#%s-*.md" % (milestone_num,)):
-            if milestonefile != milestone_file:
-                subprocess.Popen(["git","mv","-f",milestonefile,milestone_file])
-
-        
-        milestone_description = milestone_description.encode("utf-8")
-        if not os.path.exists(milestone_file) or os.stat(milestone_file).st_size != len(milestone_description + '\n'):
-            print milestone_file
-            cout = open(milestone_file,"w")
-            cout.write(milestone_description + '\n')
-            cout.close()
     
 
 if __name__ == "__main__":
-    sys.exit(dumpIssues("open","MinYi","LeslieZhu"))
+    sys.exit(gitIssues("open","MinYi","LeslieZhu"))
 
